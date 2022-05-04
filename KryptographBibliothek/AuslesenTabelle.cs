4b583d76de24965d997f3b5fb9aa4bb8bbe5aea0
Kander Akinci <k.akinci@bkgl.bgl.schule>
2022-05-04 14:16:40 +0200

    UD

---
+++ KryptographBibliothek/AuslesenTabelle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KryptographBibliothek
+{
+    public class AuslesenTabelle
+    {
+
+        public static void AuslesenTab()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
