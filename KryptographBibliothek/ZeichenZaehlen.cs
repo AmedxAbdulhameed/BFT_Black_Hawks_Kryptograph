db71678e486d6c97c42b7078bce6f574249379a2
Mustafa Hamko <mustafa.h11@gso.schule.koeln>
2022-05-04 14:40:47 +0200

    UPDATE

---
+++ KryptographBibliothek/ZeichenZaehlen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KryptographBibliothek
+{
+    public class ZeichenZaehlen
+    {
+        public static void Zeichenzaehlen()
+        {
+            Console.WriteLine("test");
+        }
+
+
+    }
+}
