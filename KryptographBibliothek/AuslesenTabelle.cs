ba993d8641eac2ac47434eb86b1f1dd050237490
Tim Gavenesch <tim.g33@gso.schule.koeln>
2022-05-11 14:33:37 +0200

    Neu

--- KryptographBibliothek/AuslesenTabelle.cs
+++ KryptographBibliothek/AuslesenTabelle.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 namespace KryptographBibliothek
 {
     public class AuslesenTabelle
@@ -7,7 +7,60 @@ namespace KryptographBibliothek
 
         public static void AuslesenTab()
         {
-            Console.WriteLine("Test");
+            Console.WriteLine("Bitte geben sie ein E für den Englischen Text oder D für den Deutschen Text");
+            String Text = Console.ReadLine();
+
+
+
+
+
+            if (Text == "D")
+            {
+
+
+                string[] text = File.ReadAllLines(@"C:\Users\tim.g33\Source\Repos\BFT_Black_Hawks_Kryptograph1\Deutsch_Wahrscheinlichkeiten.txt");
+
+                foreach (string line in text)
+                {
+                    string[] daten = line.Split(' ');
+                    string[] daten2 = daten[0].Split('\t');
+                    int nummer = int.Parse(daten[0]);
+                    string name = daten[1];
+                    double gehalt = double.Parse(daten[2]);
+
+
+                }
+           
+            
+
+
+
+            }
+            if (Text=="E")
+
+            {
+                string[] text1 = File.ReadAllLines(@"C:\Users\tim.g33\Source\Repos\BFT_Black_Hawks_Kryptograph1\Englisch_Wahrscheinlichkeiten.txt");
+
+                foreach (string text2 in text1)
+                {
+                    string[] daten = text2.Split(';');
+                    int nummer = int.Parse(daten[0]);
+                    string name = daten[1];
+                    double gehalt = double.Parse(daten[2]);
+
+
+                }
+
+
+
+
+            }
+
+
+
+
+
+
         }
     }
 }
