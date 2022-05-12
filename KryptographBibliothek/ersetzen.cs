3a8e89236d6ecf35f049352755a287b202151f6b
Aras Kasoo <aras.k5@gso.schule.koeln>
2022-05-12 11:04:01 +0200

    Mein Programm

--- KryptographBibliothek/ersetzen.cs
+++ KryptographBibliothek/ersetzen.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace KryptographBibliothek
 {
@@ -8,55 +9,7 @@ namespace KryptographBibliothek
         public static void ZeichenErsetzen()
            
         {
-            string str = "Kly ulbl KbhsZluzl Jvuayvssly pza pu Mburapvuzbtmhun buk Clyhyilpabun lpul rshyl Clyilzzlybun gbt KbhsZovjr 4, hsslykpunz mpls tpy kpl nlypunl Hrrb - Shbmglpa däoyluk klz Alzaz ulnhapc hbm, kpl cvy hsslt Jvyl Nhtly zaöylu küymal.Lilumhssz upjoa glpanltäß pza kpl Nyößl kly pualyulu Mlzawshaal, dlsjol upjoa 1 AI, zvuklyu uby 825 NI nyvß pza, cvu klulu dplklybt uby 667 NI nlubaga dlyklu röuulu, kh kly Ylza cvt Zfzalt ilslna dpyk.Pu Glpalu, pu klulu Zwplsl hbjo zjovuths ipz gb 100 NI nyvß zpuk, dlyklu oply zjoulss Wshagwyvisltl luazalolu.Kpl Rvuzvsl bualyzaüaga lydlpalyal Zwlpjolytönspjorlpalu pu Mvyt cvu BZI - Shbmdlyrlu, hsslykpunz röuulu oply uby WZ4 - Zwplsl hbznlshnlya dlyklu.Kly pualyul Zwlpjoly rhuu müy WZ5 - Zwplsl tpa ZZK - Lydlpalybunzrhyalu(khur T.2.- Zjoupaazalssl) clynyößlya dlyklu, hsslykpunz dpyk kplzlz Mlahbyl ilp Ylslhzl uvjo upjoa gby Clymünbunz zalolu, kluu lz pza lyza müy lpu gbrüumapnlz Bwkhal nlwshua.Lz islpia higbdhyalu, dpl zpjo kpl WZ5 pt Shunglpaalza zjo ersbzpc - Zwplslu khym hily qlaga zjovu nlyul tpa Zvufz Ulea - Nlu - Rvuzvsl nlspliäbnlsa dlyklu";
-
-            Console.WriteLine(str);
-            Dictionary<String, double> Tabelle = new Dictionary<string, double>();
-
-            {
-                Tabelle.Add("A", 0.0558);
-                Tabelle.Add("Ä", 0.0054);
-                Tabelle.Add("B", 0.0196);
-                Tabelle.Add("C", 0.0316);
-                Tabelle.Add("D", 0.0498);
-                Tabelle.Add("E", 0.1693);
-                Tabelle.Add("F", 0.0149);
-                Tabelle.Add("G", 0.0302);
-                Tabelle.Add("H", 0.0498);
-                Tabelle.Add("I", 0.0802);
-                Tabelle.Add("J", 0.0024);
-                Tabelle.Add("K", 0.0132);
-                Tabelle.Add("L", 0.0360);
-                Tabelle.Add("M", 0.0255);
-                Tabelle.Add("N", 0.1053);
-                Tabelle.Add("O", 0.0224);
-                Tabelle.Add("Ö", 0.0030);
-                Tabelle.Add("P", 0.0067);
-                Tabelle.Add("Q", 0.0002);
-                Tabelle.Add("R", 0.0689);
-                Tabelle.Add("ß", 0.0037);
-                Tabelle.Add("S", 0.0642);
-                Tabelle.Add("T", 0.0579);
-                Tabelle.Add("U", 0.0383);
-                Tabelle.Add("Ü", 0.0065);
-                Tabelle.Add("V", 0.0084);
-                Tabelle.Add("W", 0.0178);
-                Tabelle.Add("X", 0.0005);
-                Tabelle.Add("Y", 0.0005);
-                Tabelle.Add("Z", 0.0121);
-                
-            }
-
-
-
-
-
-
-
-
-
-
-
+         
 
 
         }
--- StartKryptograph/main.cs
+++ StartKryptograph/main.cs
@@ -1,14 +1,55 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace StartKryptograph
 {
     class main
     {
         static void Main(string[] args)
         {
-            string str = "Kly ulbl KbhsZluzl Jvuayvssly pza pu Mburapvuzbtmhun buk Clyhyilpabun lpul rshyl Clyilzzlybun gbt KbhsZovjr 4, hsslykpunz mpls tpy kpl nlypunl Hrrb - Shbmglpa däoyluk klz Alzaz ulnhapc hbm, kpl cvy hsslt Jvyl Nhtly zaöylu küymal.Lilumhssz upjoa glpanltäß pza kpl Nyößl kly pualyulu Mlzawshaal, dlsjol upjoa 1 AI, zvuklyu uby 825 NI nyvß pza, cvu klulu dplklybt uby 667 NI nlubaga dlyklu röuulu, kh kly Ylza cvt Zfzalt ilslna dpyk.Pu Glpalu, pu klulu Zwplsl hbjo zjovuths ipz gb 100 NI nyvß zpuk, dlyklu oply zjoulss Wshagwyvisltl luazalolu.Kpl Rvuzvsl bualyzaüaga lydlpalyal Zwlpjolytönspjorlpalu pu Mvyt cvu BZI - Shbmdlyrlu, hsslykpunz röuulu oply uby WZ4 - Zwplsl hbznlshnlya dlyklu.Kly pualyul Zwlpjoly rhuu müy WZ5 - Zwplsl tpa ZZK - Lydlpalybunzrhyalu(khur T.2.- Zjoupaazalssl) clynyößlya dlyklu, hsslykpunz dpyk kplzlz Mlahbyl ilp Ylslhzl uvjo upjoa gby Clymünbunz zalolu, kluu lz pza lyza müy lpu gbrüumapnlz Bwkhal nlwshua.Lz islpia higbdhyalu, dpl zpjo kpl WZ5 pt Shunglpaalza zjo ersbzpc - Zwplslu khym hily qlaga zjovu nlyul tpa Zvufz Ulea - Nlu - Rvuzvsl nlspliäbnlsa dlyklu";
+            string chiffre = "Kly ulbl";
 
-            KryptographBibliothek.Ersetzen.ZeichenErsetzen();
+            Dictionary<String, double> tabelle_chiffre = new Dictionary<string, double>();
+            tabelle_chiffre.Add("K", 1 / 7.0);
+            tabelle_chiffre.Add("L", 3 / 7.0);
+            
+
+
+            Dictionary<String, double> Capitals
+                = new Dictionary<string, double>();
+
+            {
+                Capitals.Add("A", 0.0558);
+                Capitals.Add("Ä", 0.0054);
+                Capitals.Add("B", 0.0196);
+                Capitals.Add("C", 0.0316);
+                Capitals.Add("D", 0.0498);
+                Capitals.Add("E", 0.1693);
+                Capitals.Add("F", 0.0149);
+                Capitals.Add("G", 0.0302);
+                Capitals.Add("H", 0.0498);
+                Capitals.Add("I", 0.0802);
+                Capitals.Add("J", 0.0024);
+                Capitals.Add("K", 0.0132);
+                Capitals.Add("L", 0.0360);
+                Capitals.Add("M", 0.0255);
+                Capitals.Add("N", 0.1053);
+                Capitals.Add("O", 0.0224);
+                Capitals.Add("Ö", 0.0030);
+                Capitals.Add("P", 0.0067);
+                Capitals.Add("Q", 0.0002);
+                Capitals.Add("R", 0.0689);
+                Capitals.Add("ß", 0.0037);
+                Capitals.Add("S", 0.0642);
+                Capitals.Add("T", 0.0579);
+                Capitals.Add("U", 0.0383);
+                Capitals.Add("Ü", 0.0065);
+                Capitals.Add("V", 0.0084);
+                Capitals.Add("W", 0.0178);
+                Capitals.Add("X", 0.0005);
+                Capitals.Add("Y", 0.0005);
+                Capitals.Add("Z", 0.0121);
+
+                string Klartext = KryptographBibliothek.Ersetzen.ZeichenErsetzen();
         }
     }
 }
