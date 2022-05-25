b1e4b3f5b58417fe94ff3dd08909e3e7ded1b128
romeohodeifa <89914783+romeohodeifa@users.noreply.github.com>
2022-05-25 14:19:37 +0200

    neu

--- KryptographBibliothek/Class1.cs
+++ KryptographBibliothek/Class1.cs
@@ -2,7 +2,42 @@
 
 namespace KryptographBibliothek
 {
-    public class Class1
+    public class Ausgabe
     {
+        public static string AusgabenZeichen(string)
+        {
+
+            string chiffre = "Kyl, ";
+            string klartext = "Das, ";
+
+
+            Console.WriteLine("Ergebnis");
+            Console.WriteLine("--------------------------");
+
+            for (int i = 0; i < chiffre.Length; i++)
+            {
+                if (chiffre[i] != klartext[i])
+                {
+                    Console.BackgroundColor
+                    = ConsoleColor.Red;
+
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+
+                }
+
+                Console.WriteLine(chiffre);
+                Console.WriteLine(klartext);
+
+
+            }
+
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            return klartext;
+        }
+
     }
 }
