2f1084a6b771363f3375a23281d484c660ab11a4
Cedric Wiest <89574050+DarkHavest@users.noreply.github.com>
2022-06-03 13:46:42 +0200

    Update ZeichenZaehlen.cs

--- KryptographBibliothek/ZeichenZaehlen.cs
+++ KryptographBibliothek/ZeichenZaehlen.cs
@@ -4,11 +4,83 @@ namespace KryptographBibliothek
 {
     public class ZeichenZaehlen
     {
-        public static void Zeichenzaehlen()
-        {
-            Console.WriteLine("test");
-        }
+       public static Dictionary<string,double> Zeichenzaehlen()
+{
+Dictionary<string, double> Zählen = new Dictionary<string, double>();
 
+
+
+string text = "Kly ulbl";
+text = text.ToUpper();
+
+
+
+
+//Momentanes zeichen zählen
+//Schauen ob Zeichen in der Tabelle vorhanden ist (c# check if char inf Dictionary/ c# schau ob karakter ind Dictionary)
+//Zeichen mit Wahrscheinlichkeit hinzufügen (c# add key and value to dictionary/ c# eintrag in dictionary hinzufügen)
+
+
+
+
+for (int i=0; i<text.Length; i++)
+{
+//text[i]
+
+
+
+//help start
+bool dupeCharFlag = false;
+for (int j = 0; j < i;j++)
+{
+if (text[j] == text[i])
+{
+dupeCharFlag = true;
+}
+}
+if (dupeCharFlag)
+{
+continue;
+}
+// help end
+
+
+
+Console.WriteLine(text[i]);
+int anzahl = text.Count(c => c == text[i]);
+
+
+
+if (!Zählen.ContainsKey(text[i].ToString()))
+{
+
+
+
+double Prozent = anzahl * 100/ (text.Length) ;
+
+
+
+
+Console.WriteLine("Der Prozent werd im Text ist:" + Prozent + "%");
+
+
+
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+return Zählen;
+}
 
     }
 }
