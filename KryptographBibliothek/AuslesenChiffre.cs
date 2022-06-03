8d20ef69b5e8c3bc36f422a15229af3992444226
Key20030 <89571940+Key20030@users.noreply.github.com>
2022-06-03 14:06:25 +0200

    Update AuslesenChiffre.cs

--- KryptographBibliothek/AuslesenChiffre.cs
+++ KryptographBibliothek/AuslesenChiffre.cs
@@ -8,22 +8,18 @@ namespace KryptographBibliothek
     {
          public static void Auslesen( )
          {
-            
+            //wie gebe ich durch einen pfad den inhalt einer textdatei in c# aus?
 
-            
-            foreach (string line in System.IO.File.ReadLines(@"c:\Chiffre.txt"))
-            {
-              
+            //------------------------------------------------
 
-                
-            }
+            string text = System.IO.File.ReadAllText(pfad);
+
+            string chiffre =   text;
 
 
+            //-----------------------------------
 
-
-            return;
-
-
+            return chiffre;
 
          }
 
